48f3d239f843dd49c47c1f388b948836f15bd7be
Osama Bugshan <osama.bugshan@kcl.ac.uk>
2023-11-12 23:44:47 +0000

    new try

--- things/views.py
+++ things/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Thing
+
 def home(request):
-    return render(request, 'home.html')
+    # Query all Thing objects
+    things = Thing.objects.all()
+    # Pass them to the context
+    context = {'things': things}
+    return render(request, 'home.html', context)
+
 
