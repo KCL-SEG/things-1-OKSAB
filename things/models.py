038ca46e7d388c628d8a5a5dc1ed788c85a2f934
Osama Bugshan <osama.bugshan@kcl.ac.uk>
2023-12-02 09:07:22 +0000

    Try solution

--- things/models.py
+++ things/models.py
@@ -2,9 +2,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=35, unique=True)
-    description = models.CharField(max_length=120, blank=True)
-    quantity = models.IntegerField(
-        validators=[MinValueValidator(0),MaxValueValidator(50)]
-    )
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.CharField(max_length=120, blank=True, unique=False)
+    quantity = models.IntegerField(unique=False, validators=[MinValueValidator(0),MaxValueValidator(100)])
